1f5cfe87492e2d754f41829b3b19c4e8e173220f
Matei Stefanescu <mstefanescu@optaros.com>
2017-07-10 11:08:13 +0300

    refactored a bit

--- src/main/java/com/optaros/tokendecoder/Decoder.java
+++ src/main/java/com/optaros/tokendecoder/Decoder.java
@@ -1,9 +1,12 @@
 package com.optaros.tokendecoder;
+import com.typesafe.config.ConfigFactory;
 import org.apache.commons.codec.binary.Base64;
 
 import javax.crypto.Cipher;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
+
+import com.typesafe.config.Config;
 
 /**
  * AES-CBC token decoder
@@ -11,10 +14,8 @@ import javax.crypto.spec.SecretKeySpec;
  */
 public class Decoder {
 
-    public static void main(String[] args) throws Exception {
-        String encryptedToken = "rT7UpXjSMfFqK2s9S2ZsZA3hqGp3+gXiVWd48biLXMGP74VbMruHhBKncE+SbYHIHk398M2778EgkN+eJBfFtGdGfwfhBqlfFFU2WSonQmo0mAQ+bzB7Nviu0kpMmGNaDvy6ggUArnhlcn9atrFbGOHz5c02C4UNgY2+puohUg3BvCkqtInqvr71wQ6MUBwthZ0nsO25x5Qoi8vo8W9ilg==:JH+MhcKL14RnT+X9lkNfQA==";
-        String encryptionKey = "b3B0YXJvczEyMzQ1Njc4OQ==";
-
+    public String decode(String encryptedToken, String encryptionKey) throws Exception
+    {
         String[] parts = encryptedToken.split(":");
         String payloadString = parts[0];
         String ivString = parts[1];
@@ -24,17 +25,22 @@ public class Decoder {
         byte[] binaryEncryptedKey = decoder.decode(encryptionKey);
         byte[] ivBytes = decoder.decode(ivString);
 
-        System.out.println(ivBytes.length);
-        System.out.println(payloadBytes.length);
-
         IvParameterSpec iv = new IvParameterSpec(ivBytes);
         SecretKeySpec keySpec = new SecretKeySpec(binaryEncryptedKey, "AES");
         Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
         cipher.init(Cipher.DECRYPT_MODE, keySpec, iv);
 
         byte[] decryptedPayload = cipher.doFinal(payloadBytes);
-        String decryptedString = new String(decryptedPayload, "UTF-8");
+        return new String(decryptedPayload, "UTF-8");
+    }
 
-        System.out.println("Decoded: " + decryptedString);
+
+    public static void main(String[] args) throws Exception {
+        String encryptedToken = "rT7UpXjSMfFqK2s9S2ZsZA3hqGp3+gXiVWd48biLXMGP74VbMruHhBKncE+SbYHIHk398M2778EgkN+eJBfFtGdGfwfhBqlfFFU2WSonQmo0mAQ+bzB7Nviu0kpMmGNaDvy6ggUArnhlcn9atrFbGOHz5c02C4UNgY2+puohUg3BvCkqtInqvr71wQ6MUBwthZ0nsO25x5Qoi8vo8W9ilg==:JH+MhcKL14RnT+X9lkNfQA==";
+
+        Config config = ConfigFactory.load();
+        Decoder decoder = new Decoder();
+
+        System.out.println("Decoded: " + decoder.decode(encryptedToken, config.getString("decoder.key")));
     }
 }
