009688ced7e1834a5bf8438aa4492dfe8e1a9a18
Matei Stefanescu <mstefanescu@optaros.com>
2017-07-10 11:34:39 +0300

    refactoring a bit

--- src/main/java/com/optaros/tokendecoder/Decoder.java
+++ src/main/java/com/optaros/tokendecoder/Decoder.java
@@ -6,6 +6,7 @@ import javax.crypto.Cipher;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
+import com.typesafe.config.ConfigFactory;
 import com.typesafe.config.Config;
 
 /**
